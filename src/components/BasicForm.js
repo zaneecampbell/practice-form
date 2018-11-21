fb69cf66014cbdc47478d435065ce36c8402f7fd
Zane Campbell <zanegitinit@gmail.com>
2018-11-20 18:40:17 -0800

    Added Material-UI styling

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 export default class BasicForm extends React.Component {
     state = {
@@ -28,34 +30,45 @@ export default class BasicForm extends React.Component {
 
     handleReset = () => {
         document.getElementById('return').style.display = 'none';;
-        this.setState (() => ({
+        this.setState(() => ({
             name: '',
             age: ''
         }));
         document.getElementById('name').style.display = 'inline';
-        document.getElementById('nameField').value='';
-        document.getElementById('ageField').value='';
+        document.getElementById('nameField').value = '';
+        document.getElementById('ageField').value = '';
     };
 
     render() {
         return (
             <div>
-                <h1>Basic Form</h1>
-                <form className='name' id='name' onSubmit={this.handleName}>
-                    <label>What is your name? </label>
-                    <input type='text' id='nameField' autoComplete='off' autoFocus></input>
-                    <button>Next</button>
-                </form>
-                <form className='age' id='age' onSubmit={this.handleAge}>
-                    <label>How old are you? </label>
-                    <input id='ageField'></input>
-                    <button>Next</button>
-                </form>
-                <div className='return' id='return'>
-                    <h2>Hello, {this.state.name}.</h2>
-                    <h2>You are {this.state.age} years old.</h2>
-                    <button onClick={this.handleReset}>Reset</button>
-                </div>
+                <Grid
+                    container
+                    direction="column"
+                    justify="center"
+                    alignItems="center"
+                >
+                    <Paper style={{ padding: 20 }}>
+                        <Grid style={{ textAlign: 'center' }}>
+                            <h1>Basic Form</h1>
+                        </Grid>
+                        <form className='name' id='name' onSubmit={this.handleName}>
+                            <label>What is your name? </label>
+                            <input type='text' id='nameField' autoComplete='off' autoFocus></input>
+                            <button>Next</button>
+                        </form>
+                        <form className='age' id='age' onSubmit={this.handleAge}>
+                            <label>How old are you? </label>
+                            <input id='ageField'></input>
+                            <button>Next</button>
+                        </form>
+                        <div className='return' id='return'>
+                            <h3>Hello, {this.state.name}.</h3>
+                            <h3>You are {this.state.age} years old.</h3>
+                            <button onClick={this.handleReset}>Reset</button>
+                        </div>
+                    </Paper>
+                </Grid>
             </div>
         )
     }
